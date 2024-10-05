a23392b7cee5d4e061ff5cf917796e47873a761e
James Butcher <jmsbutcher1576@gmail.com>
2024-10-05 09:51:56 -0400

    Cleaned up commented out code and unnecessary files

--- website/app.py
+++ website/app.py
@@ -3,11 +3,8 @@ James Butcher
 4/18/24
 """
 from flask import Flask
-from .cms import DB_NAME, DB_URI, db, ContentPage, Topic
-from .kb_graph import n, load_graph, graph, get_all_topics, \
-    isolate_last_part_of_URI, triple_to_text
+from .kb_graph import load_graph, get_all_topics
 from .views import views
-from os import path
 
 
 def create_app():
@@ -18,54 +15,11 @@ def create_app():
     app = Flask(__name__)
     app.register_blueprint(views, url_prefix='/')
 
-
     # Current topics:
     topics = get_all_topics()
     print("\nCurrent topics:")
     for t in topics:
         print(t)
     print()
-
-
-    # Initalize Database 
-    # app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
-    # db.init_app(app)
-    # with app.app_context():
-
-    #     db.drop_all()
-    #     db.create_all()
-
-    #     # Current topics:
-    #     topics = db.session.execute(db.select(Topic)).scalars()
-    #     print("\nCurrent topics:")
-    #     for t in topics:
-    #         print(t.name)
-    #     print()
-
-    #     # Add any topics not yet added to the database from the RDF graph
-    #     topics = get_all_topics()
-
-    #     existing_topics = set([t.name for t in Topic.query.all()])
-    #     for topic in topics:
-    #         if topic not in existing_topics:
-
-    #             new_topic = Topic(
-    #                 name=topic, 
-    #                 textname=topic.replace("_", " ").title(),
-    #                 description="[description]"
-    #                 )
-                
-    #             db.session.add(new_topic)
-
-        
-    #     #Topic.query.filter(Topic.name == "Topic 2").delete()
-
-    #     db.session.commit()
-
-        
-
-        # Add any custom stuff to database before startup
-
-
 
     return app
--- website/common.py
+++ website/common.py
@@ -4,6 +4,4 @@ import os
 def get_website_dir():
     dir = os.path.dirname(os.path.abspath(__file__))
     dir = dir.replace("\\\\wsl.localhost\\", "\\\\")
-    #print("Website path:", dir, "\n")
-
     return dir
